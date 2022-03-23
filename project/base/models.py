e749bd43627f7ae5fa985ebe98f76b9fefa0b1bb
Mateusz Łuczyński <kptw1@wp.pl>
2022-03-23 14:33:58 +0100

    CRUD rooms

--- project/base/models.py
+++ project/base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -15,10 +16,13 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True) # cant be blink
-    #participants =
+    description = models.TextField(null=True, blank=True)  # cant be blink
+    # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
--- project/base/views.py
+++ project/base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
+
 # Create your views here.
 
 rooms = [
@@ -8,10 +10,12 @@ rooms = [
     {'id': 3, 'name': 'Dev!'},
 ]
 
+
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
@@ -19,6 +23,36 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
 def create_room(request):
-    context = {}
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
